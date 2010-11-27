c89b4f4791ce85a0bb64123a59e73dfb3e8e5231
kalchas <james@nearlysensical.com>
2010-11-26 23:07:37 -0800

    Added licensing information.

--- mailchimp_widget.php
+++ mailchimp_widget.php
@@ -6,11 +6,28 @@ Description:
 Author: James Lafferty
 Version: 0.1
 Author URI: https://github.com/kalchas
+License: GPL2
+*/
+
+/*  Copyright 2010  James Lafferty  (email : james@nearlysensical.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 /**
  * Set up the autoloader.
-*/
+ */
 
 set_include_path(get_include_path() . PATH_SEPARATOR . realpath(dirname(__FILE__) . '/lib/'));
 
